1eb34526d0f71ec4f9d31230eda581f9085b40e4
Jess Hines <jess.hines@fmr.com>
2020-12-08 14:24:13 -0700

    Copy to gatsby-ssr.js

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -2,11 +2,6 @@ import React from 'react'
 import { withTheme } from 'styled-components'
 import Layout from './src/components/Layout'
 
-/*
-  runs on the browser after Gatsby has been loaded
-  Usually also need to copy into gatsby-ssr.js
-*/
-
 export function wrapPageElement({element, props}) {
   return <Layout {...props}>{element}</Layout>
 
