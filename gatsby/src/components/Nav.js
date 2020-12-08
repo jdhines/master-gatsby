2b9dd873efb22dcefa8c2d077410efe885bcffed
Jess Hines <jess.hines@fmr.com>
2020-12-08 13:30:47 -0700

    Update nav with Link instead of anchor

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,14 +1,15 @@
+import { Link } from 'gatsby'
 import React from 'react'
 
 export default function Nav() {
   return (
     <nav>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/beers">Beers</a></li>
-        <li><a href="/pizzas">Pizzas</a></li>
-        <li><a href="/order">Order</a></li>
-        <li><a href="/slicemaster">Slicemaster</a></li>
+        <li><Link to="/">LOGO</Link></li>
+        <li><Link to="/">Hot Now</Link></li>
+        <li><Link to="/pizzas">Pizzas</Link></li>
+        <li><Link to="/slicemaster">Slicemaster</Link></li>
+        <li><Link to="/order">Order Ahead</Link></li>
       </ul>
     </nav>
   )
