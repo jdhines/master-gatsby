0806702119af3dbdbb50d79dc9f2734ff1bec529
Jess Hines <jess.hines@fmr.com>
2020-12-08 15:00:49 -0700

    Add Global Styles

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { withTheme } from 'styled-components'
 import Layout from './src/components/Layout'
-
 /*
   runs on the browser after Gatsby has been loaded
   Usually also need to copy into gatsby-ssr.js
