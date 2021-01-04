c2a99372d59bbd71d898bcfcc371f2401705b5b6
Jess Hines <jess.hines@fmr.com>
2021-01-04 13:10:25 -0700

    Style logo, nav, and layout

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -2,16 +2,42 @@ import React from 'react'
 import Footer from './Footer'
 import Nav from './Nav'
 import 'normalize.css'
+import styled from 'styled-components'
 import GlobalStyles from '../styles/GlobalStyles'
 import Typography from '../styles/Typography'
+import stripes from '../assets/images/stripes.svg'
+
+const SiteBorderStyles = styled.div`
+  max-width: 1000px;
+  margin: 12rem auto 4rem auto;
+  margin-top: clamp(2rem, 10vw, 12rem);
+  background: white url(${stripes});
+  border: 5px solid white;
+  padding: 5px;
+  padding: clamp(5px, 1vw, 25px);
+  box-shadow: 0 0 10px 3px rgba(0,0,0,0.25);
+  @media(max-width: 1100px) {
+    margin-left: 1.5rem;
+    margin-right: 1.5rem;
+  }
+`;
+const ContentStyles = styled.div`
+  background: white;
+  padding: 2rem;
+`;
+
 export default function Layout({children}) {
   return (
-    <div>
+    <>
       <GlobalStyles />
       <Typography />
-      <Nav />
-      {children}
-      <Footer />
-    </div>
+      <SiteBorderStyles>
+        <ContentStyles>
+          <Nav />
+          {children}
+          <Footer />
+        </ContentStyles>
+      </SiteBorderStyles>
+    </>
   )
 }
